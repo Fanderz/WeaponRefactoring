e050b18b59d4ff0309f4def80d955c4c70b80525
Fanderz <135126601+Fanderz@users.noreply.github.com>
2025-03-08 10:06:23 +0300

    feat: refact code
    - add readonly
    - add exceptions
    - removing magic data

--- Weapon.cs
+++ Weapon.cs
@@ -1,13 +1,33 @@
+using System;
 
 class Weapon
 {
-    private int _damage;
+    private readonly int _damage;
+    private readonly int _bulletsPerShot;
+
     private int _bullets;
+
+    public Weapon(int damageSize, int gunMagazineSize)
+    {
+        if (damageSize <= 0 || gunMagazineSize <= 0)
+            return new ArgumentOutOfRangeException();
+
+        _damage = damageSize;
+        _bullets = gunMagazineSize;
+
+        _bulletsPerShot = 1;
+    }
 
     public void Fire(Player player)
     {
+        if (player == null)
+            return new ArgumentNullException();
+
+        if (_bullets <= 0)
+            return new InvalidOperationException();
+
         player.TakeDamage(_damage);
-        _bullets -= 1;
+        _bullets -= _bulletsPerShot;
     }
 }
 
@@ -15,8 +35,19 @@ class Player
 {
     private int _health;
 
+    public Player(int healthSize)
+    {
+        if (healthSize <= 0)
+            return new ArgumentOutOfRangeException();
+
+        _health = healthSize;
+    }
+
     public void TakeDamage(int damage)
     {
+        if (damage <= 0)
+            return new ArgumentOutOfRangeException();
+
         if (_health >= damage)
             _health -= damage;
     }
@@ -24,10 +55,18 @@ class Player
 
 class Bot
 {
-    private Weapon _weapon;
+    private readonly Weapon _weapon;
+
+    public Bot()
+    {
+        _weapon = new Weapon();
+    }
 
     public void OnSeePlayer(Player player)
     {
+        if (player == null)
+            return new ArgumentNullException();
+
         _weapon.Fire(player);
     }
 }
